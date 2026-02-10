fcd19dc8de6b68b16197f9acc2c5d898d84b2d23
golibjon94 <golib94jon@gmail.com>
2026-02-10 19:00:54 +0500

    Introduce loading indicator for navigation events
    - Added a `MatProgressBar` to the top of the application for indicating navigation loading states.
    - Subscribed to Angular Router events to dynamically toggle the `loading` state.
    - Styled the progress bar with fixed positioning at the top for visibility during route changes.

--- kaidzen-front/src/app/app.ts
+++ kaidzen-front/src/app/app.ts
@@ -1,22 +1,38 @@
 import { Component, signal, inject, OnInit } from '@angular/core';
 import { AuthService } from './core/services/auth.service';
 import { AuthStore } from './core/stores/auth.store';
-import {RouterOutlet} from '@angular/router';
+import { Router, NavigationStart, NavigationEnd, NavigationCancel, NavigationError, RouterOutlet } from '@angular/router';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    MatProgressBarModule
   ],
   styleUrl: './app.css'
 })
 export class App implements OnInit {
   protected readonly title = signal('kaidzen-front');
+  loading = signal(false);
   private authService = inject(AuthService);
   private authStore = inject(AuthStore);
+  private router = inject(Router);
 
   ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        this.loading.set(true);
+      } else if (
+        event instanceof NavigationEnd ||
+        event instanceof NavigationCancel ||
+        event instanceof NavigationError
+      ) {
+        this.loading.set(false);
+      }
+    });
+
     if (this.authStore.isAuthenticated()) {
       this.authService.getMe().subscribe({
         error: () => {
